8d059340b55195816b541b6beab35da1117e94bf
taifunn <milena.oberzig@wp.pl>
2025-11-13 15:06:05 +0100

    .

--- 3rd/cpp_stl/list4/4.cpp
+++ 3rd/cpp_stl/list4/4.cpp
@@ -4,16 +4,19 @@
 #include <list>
 #include <set>
 #include <string>
+#include <functional>
 #include <stdexcept>
 using namespace std;
 
 int main(){
-    auto newton = [&](auto &&self, int n, int k) -> long long {
+    function<long long(int,int)> newton;
+
+    newton = [&](int n, int k) -> long long {
         if(n<0 || k<0 || k>n) throw invalid_argument("powinno byc 0<=k<=n");
         if(k==0 || k==n) return 1;
 
         k = min(k, n - k);
-        return self(self, n - 1, k - 1) + self(self, n - 1, k);
+        return newton(n - 1, k - 1) + newton(n - 1, k);
     };
 
     vector<std::pair<int,int>> tests = {
@@ -28,7 +31,7 @@ int main(){
     for (auto [n, k] : tests) {
         try {
             std::cout << "C(" << n << "," << k << ") = "
-                      << newton(newton, n, k) << "\n";
+                      << newton(n, k) << "\n";
         } catch (const std::exception& e) {
             std::cout << "C(" << n << "," << k << ") -> wyjatek: "
                       << e.what() << "\n";
