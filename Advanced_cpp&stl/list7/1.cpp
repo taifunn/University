b2fe039f95666d03d8da654aae46593c0369ed46
taifunn <milena.oberzig@wp.pl>
2025-12-10 03:29:29 +0100

    .

---
+++ Advanced_cpp&stl/list7/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <complex>
+#include <cmath>
+
+using namespace std;
+using cd = complex<double>;
+
+const double euler_gamma = 0.57721566490153286060;
+
+cd gamma_euler(cd z, int N){
+    
+}
